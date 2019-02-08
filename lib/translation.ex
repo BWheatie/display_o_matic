337c6fe299085d90e91a14d794a618edb42d4726
Ben Wheat <bwheatie@gmail.com>
2019-02-08 11:30:05 -0600

    Padding

--- lib/translation.ex
+++ lib/translation.ex
@@ -2,29 +2,34 @@ defmodule Scenic.Translation do
   @viewport :display_o_matic
             |> Application.get_env(:viewport)
             |> Map.get(:size)
-  @viewportx elem(@viewport, 0)
-  @viewporty elem(@viewport, 1)
+  # @viewportx elem(@viewport, 0)
+  # @viewporty elem(@viewport, 1)
 
-  def padding(pix) do
-    x = padding_bottom(pix) - padding_top(pix)
-    y = padding_right(pix) - padding_left(pix)
+  def padding(pix, points \\ @viewport) do
+    case points do
+      @viewport ->
+        x = padding_bottom(pix, elem(points, 0)) - padding_top(pix, 0)
+        y = padding_right(pix, elem(points, 1)) - padding_left(pix, 0)
 
-    {x, y}
+        {x, y}
+
+      points ->
+        x = padding_bottom(pix, elem(points, 0)) - padding_top(pix, elem(points, 0))
+        y = padding_right(pix, elem(points, 1)) - padding_left(pix, elem(points, 1))
+
+        {x, y}
+    end
   end
 
-  def padding_top(pix) do
-    pix
-  end
+  def padding_top(pix, point) when point == 0, do: pix
+  def padding_top(pix, point), do: point + pix
 
-  def padding_bottom(pix) do
-    @viewportx - pix
-  end
+  def padding_bottom(pix, point) when point == 0, do: pix
+  def padding_bottom(pix, point), do: point - pix
 
-  def padding_left(pix) do
-    pix
-  end
+  def padding_left(pix, point) when point == 0, do: pix
+  def padding_left(pix, point), do: point + pix
 
-  def padding_right(pix) do
-    @viewporty - pix
-  end
+  def padding_right(pix, point) when point == 0, do: pix
+  def padding_right(pix, point), do: point - pix
 end
